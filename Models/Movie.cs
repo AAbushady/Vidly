4344395346b90988b6f59373d714ea76d5faca9b
Alex Abushady <aabushady214@gmail.com>
2019-12-04 14:32:26 -0500

    Updated CustomerForm so that when a new customer is being worked on New is displayed and when an existing Customer is being worked on Edit is displayed. Gave user the ability to add and edit the movies within the database.

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -12,13 +12,16 @@ namespace Vidly.Models
         public int Id { get; set; }
         [Required]
         public string Name { get; set; }
-        [Required]
         public Genre Genre { get; set; }
+        [Display(Name ="Genre")]
         public short GenreId { get; set; }
+
+        [Display(Name = "Release Date")]
         [Required]
         public DateTime? ReleaseDate { get; set; }
         [Required]
         public DateTime? DateAdded { get; set; }
+        [Display(Name = "Number in Stock")]
         [Required]
         public byte InStock { get; set; }
 
