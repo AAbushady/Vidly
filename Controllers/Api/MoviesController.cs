af10b40fecf9120c032a7db76caea69e345f4823
Alex Abushady <aabushady214@gmail.com>
2019-12-26 12:08:41 -0500

    Ajax call to New Rental api now allows for keeping track of who rents what, and what is rented.

--- Controllers/Api/MoviesController.cs
+++ Controllers/Api/MoviesController.cs
@@ -19,11 +19,18 @@ namespace Vidly.Controllers.Api
         }
 
         // GET /api/movies
-        public IHttpActionResult GetMovies()
+        public IHttpActionResult GetMovies(string query = null)
         {
-            var movieDtos = _context.Movies
+            var moviesQuery = _context.Movies
                 .Include(m => m.Genre)
-                .ToList()
+                .Where(m => m.NumberAvailable > 0);
+
+            if (!String.IsNullOrWhiteSpace(query))
+            {
+                moviesQuery = moviesQuery.Where(m => m.Name.Contains(query));
+            }
+
+            var movieDtos = moviesQuery
                 .Select(Mapper.Map<Movie, MovieDto>);
 
             return Ok(movieDtos);
